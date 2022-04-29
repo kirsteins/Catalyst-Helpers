9c4b883f98e0466cabb96f6fde7c638c99fee5d6
Jānis Kiršteins <janis.kirsteins@gmail.com>
2022-04-29 12:53:58 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+import PackageDescription
+
+let package = Package(
+    name: "CatalystHelpers",
+    products: [
+        .library(name: "CatalystHelpers", targets: ["CatalystHelpers"]),
+    ],
+    targets: [
+        .target(name: "CatalystHelpers"),
+    ]
+)
